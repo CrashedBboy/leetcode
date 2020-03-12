ae48b8344ec66e26ef4591ba5ee09282867bf929
CrashedBboy <crashedbboy@gmail.com>
2020-03-12 15:53:35 +0800

    226. Invert Binary Tree

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ The journey of grinding [LeetCode](https://leetcode.com/).
 | 617 | [Merge Two Binary Trees](https://leetcode.com/problems/merge-two-binary-trees) | Easy | V |
 | 104 | [Maximum Depth of Binary Tree](https://leetcode.com/problems/maximum-depth-of-binary-tree) | Easy | V |
 | 136 | [Single Number](https://leetcode.com/problems/single-number) | Easy | V |
-| 226 | [Invert Binary Tree](https://leetcode.com/problems/invert-binary-tree) | Easy |  |
+| 226 | [Invert Binary Tree](https://leetcode.com/problems/invert-binary-tree) | Easy | V |
 | 206 | [Reverse Linked List](https://leetcode.com/problems/reverse-linked-list) | Easy |  |
 | 283 | [Move Zeroes](https://leetcode.com/problems/move-zeroes) | Easy |  |
 | 169 | [Majority Element](https://leetcode.com/problems/majority-element) | Easy |  |
---
+++ algorithms/easy/InvertBinaryTree.cpp
@@ -0,0 +1,52 @@
+// Source : https://leetcode.com/problems/maximum-depth-of-binary-tree/
+// Author : CrashedBboy
+// Date   : 2020-03-11
+
+// Questions
+/********************************************************************************** 
+
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+**********************************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        
+        if (root == NULL) { return root; }
+        
+        std::swap(root->right, root->left);
+
+        root->left = invertTree(root->left);
+        root->right = invertTree(root->right);
+
+        return root;
+
+    }
+};
