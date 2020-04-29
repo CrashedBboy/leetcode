7518662d686343aca1ad0a49c055256faed94387
CrashedBboy <crashedbboy@gmail.com>
2020-04-29 16:10:38 +0800

    62. Unique Paths (recursive DP)

---
+++ algorithms/medium/UniquePaths_1.cpp
@@ -0,0 +1,83 @@
+// Source : https://leetcode.com/problems/unique-paths/
+// Author : CrashedBboy
+// Date   : 2020-04-29
+
+// Questions
+/********************************************************************************** 
+
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the 
+diagram below).
+
+The robot can only move either down or right at any point in time.
+The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in 
+the diagram below).
+
+How many possible unique paths are there?
+
+[image] https://assets.leetcode.com/uploads/2018/10/22/robot_maze.png
+Above is a 7 x 3 grid. How many possible unique paths are there?
+
+Example 1:
+
+Input: m = 3, n = 2
+Output: 3
+
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+
+Example 2:
+
+Input: m = 7, n = 3
+Output: 28
+ 
+Constraints:
+
+* 1 <= m, n <= 100
+* It's guaranteed that the answer will be less than or equal to 2 * 10 ^ 9.
+
+**********************************************************************************/
+
+// [idea]
+// dynamic programming with memoization
+
+// the answer <= 2e9 (approximately integer limit), so we can use int to store result
+
+class Solution {
+
+private:
+
+    int pathNumber(int m, int n, int currentM, int currentN, vector<vector<int>>& memoization) {
+
+        if (memoization[currentM][currentN] != -1) {
+            return memoization[currentM][currentN];
+        }
+
+        if (currentM == m || currentN == n) {
+
+            memoization[currentM][currentN] = 1;
+        
+        } else {
+
+            memoization[currentM][currentN] = pathNumber(m, n, currentM, currentN + 1, memoization)
+                        +  pathNumber(m, n, currentM + 1, currentN, memoization);
+        }
+
+        return memoization[currentM][currentN];
+    }
+
+
+public:
+    int uniquePaths(int m, int n) {
+
+        if (m <= 0 || n <= 0) {
+            return 0;
+        }
+
+        vector<vector<int>> memoization (m, vector<int>(n, -1));
+
+        return pathNumber(m-1, n-1, 0, 0, memoization);
+    }
+};
