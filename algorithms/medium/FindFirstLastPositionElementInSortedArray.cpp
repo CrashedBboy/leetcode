62ef07bf27a67a88cb2eed5c1b3b9c30da320d53
CrashedBboy <crashedbboy@gmail.com>
2021-03-16 04:03:10 +0800

    _

---
+++ algorithms/medium/FindFirstLastPositionElementInSortedArray.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+// binary search:
+// if the center < target, return: right half execution result
+// if the center > target, return: left half execution result
+// if the center == target, return: both left and right result
+// !! always return 2 numbers
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+    }
+
+private:
+    int searchTarget(vector<int>& nums)
+};
+
+int main () {
+
+    vector<int> nums = [5, 7, 7, 8, 8, 10];
+    int target = 8;
+
+    return 0;
+}
