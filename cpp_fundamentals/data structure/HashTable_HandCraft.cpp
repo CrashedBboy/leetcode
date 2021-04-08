2e7c84980b670d5045878b3845cbafdd23653adb
CrashedBboy <crashedbboy@gmail.com>
2021-04-08 19:19:00 +0800

    HashTable API

---
+++ cpp_fundamentals/data structure/HashTable_HandCraft.cpp
@@ -0,0 +1,43 @@
+#include <utility>
+#include <iostream>
+#include <list>
+#include <vector>
+
+class HashTable {
+
+    public:
+
+        HashTable() {
+
+        }
+
+        void insert(int key, int value) {
+
+
+        }
+
+        int search(int key) {
+
+        }
+
+        void erase(int key) {
+
+
+        }
+
+        int size() {
+
+        }
+
+        void clear() {
+
+        }
+
+    private:
+
+        int BUCKET_COUNT;
+
+        int hash() {
+
+        }
+};
