f6df356a6d7c6920c91dddcbc32331afde6b81d0
CrashedBboy <crashedbboy@gmail.com>
2021-04-11 22:53:24 +0800

    C fundamental: pointer of function

--- cpp_fundamentals/language usage/FunctionPointer.cpp
+++ cpp_fundamentals/language usage/FunctionPointer.cpp
@@ -6,11 +6,24 @@ int add(int a, int b) {
 	return a + b;
 }
 
+int my_multiply(int a, int b) {
+	return a * b * (-1);
+}
+
+// pass pointer of function as parameter
+int multiply(int a, int b, int (*custom_func)(int, int)) {
+
+	return (*custom_func)(a, b);
+}
+
 int main() {
 
 	int (*func_ptr)(int, int) = &add;
-	int result = (*func_ptr)(1, 2);
+	int result1 = (*func_ptr)(1, 2);
 
-	cout << result << endl;
+	int result2 = multiply(2, 3, &my_multiply);
+
+	cout << result1 << endl;
+	cout << result2 << endl;
 	return 0;
 }
